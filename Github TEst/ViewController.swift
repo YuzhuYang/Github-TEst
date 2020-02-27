59056e931d3f3fac1914b3d0b547a92bb681419a
Yuzhu Yang <Junehouse@outlook.com>
2020-02-27 14:12:18 -0800

    Initial Commit

---
+++ Github TEst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github TEst
+//
+//  Created by Yuzhu Yang on 2/27/20.
+//  Copyright © 2020 June. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
